3f8be9d4c4076715e4f2a306d5e2a01388b179e3
r0nk <r0nk@simplecpu.com>
2023-05-27 17:31:26 -0500

    added profiler to test.sh

--- test.sh
+++ test.sh
@@ -7,12 +7,14 @@
 #fi
 
 echo "{\"uid\":\"$RANDOM\",\"kpairs\":["
-cat little_word_list.txt | while read line; 
+cat little_word_list.txt | while read line;
 do
 	echo "{\"key\":\"$line\",\"value\":\"$RANDOM\"},"
-done	
+done
 echo "{\"key\":\"$line\",\"value\":\"$RANDOM\"}"
 echo "]}"
 
 #while true; do ./test.sh | http http://127.0.0.1:5000 ; done
+
+python -m cProfile ./entropydb.py | head -n 20 > profile.txt ; cat profile.txt
 
