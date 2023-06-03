efaf476f93a7aa888e168a0896d37aa50f089c4c
r0nk <r0nk@simplecpu.com>
2023-06-03 02:19:51 -0500

    modified tests

--- test.sh
+++ test.sh
@@ -1,24 +1,4 @@
 #!/bin/bash
 
-#if [ $[ $RANDOM % 2 ] = 0 ]; then
-#cat test.json  | sed "s/REPLACEME/$RANDOM/g" | http POST http://127.0.0.1:5000
-#else
-#	cat test.json  | sed "s/REPLACEME/$RANDOM/g;s/rep2/$RANDOM/g" | http POST http://127.0.0.1:5000
-#fi
-
-echo "{\"uid\":\"$RANDOM\",\"kpairs\":["
-cat little_word_list.txt | while read line;
-do
-	if [ $[ $RANDOM % 2 ] = 0 ]; then
-		echo "{\"key\":\"$line\",\"value\":\"$RANDOM\"},"
-	else
-		echo "{\"key\":\"$line\",\"value\":\"SAME\"},"
-	fi
-done
-echo "{\"key\":\"$line\",\"value\":\"$RANDOM\"}"
-echo "]}"
-
-#while true; do ./test.sh | http http://127.0.0.1:5000 ; done
-
-#python -m cProfile ./entropydb.py | head -n 20 > profile.txt ; cat profile.txt
-
+rm database.db
+for i in $(seq 1 100); do ./gen_json.sh > test.json ; ./entropydb.py ; done
